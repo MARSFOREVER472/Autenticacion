9ef04de5e56a046ae7767ce153ba644f0c57239b
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-06-21 11:09:56 -0400

    Se mueve código fuente a la raíz.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -9,9 +9,15 @@ class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @return string/null
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo($request)
     {
-        return $request->expectsJson() ? null : route('login');
+        if (! $request->expectsJson())
+        {
+            return route('login');
+        }
     }
 }
