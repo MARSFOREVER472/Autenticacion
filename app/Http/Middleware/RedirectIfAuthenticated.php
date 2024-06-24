a8f37319ff02177600fafff663a8487656c786ee
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-06-24 11:40:35 -0400

    Se mueve código fuente a la raíz.

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -6,14 +6,16 @@ use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Symfony\Component\HttpFoundation\Response;
 
 class RedirectIfAuthenticated
 {
     /**
      * Handle an incoming request.
      *
+     * @param \Illuminate\Http\Request $request
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param string/null ...$guards
+     * @return \Illuminate\Http\Response/ \Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next, string ...$guards): Response
     {
