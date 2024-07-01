a8f59c48d06b343d019b0e4003522b606fc3c956
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-07-01 11:20:30 -0400

    Configured Database Connections.

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,29 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/signup', [App\Http\Controllers\Api\AuthController::class, 'signup']);
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->get('/status', [App\Http\Controllers\Api\AuthController::class, 'status']);
+Route::group([
+    'prefix' => 'company/{id}',
+    'middleware' => ['auth:sanctum','api']
+  ], function () {
+      Route::middleware('auth:sanctum')->get('/data_documentos', [App\Http\Controllers\Api\SGIController::class, 'getDataDocumentos']);
+  });
