0710d92d25651d86796b2b232798e7073461df85
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-07-09 23:17:47 -0400

    Se mueve código fuente a la raíz.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,17 +8,22 @@ class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
         //
     }
 }
+
--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -9,11 +9,14 @@ class BroadcastServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
         Broadcast::routes();
 
         require base_path('routes/channels.php');
     }
 }
+
